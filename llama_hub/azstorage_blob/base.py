14363a639f34e32864750c48549e10646b51ee50
Rivaaj Jumna <50959956+rivms@users.noreply.github.com>
2023-07-31 13:11:32 +0800

    Blob metadata test

--- llama_hub/azstorage_blob/base.py
+++ llama_hub/azstorage_blob/base.py
@@ -39,7 +39,7 @@ class AzStorageBlobReader(BaseReader):
         credential (Union[str, Dict[str, str], AzureNamedKeyCredential, AzureSasCredential, TokenCredential, None] = None):
             The credentials with which to authenticate. This is optional if the account URL already has a SAS token.
     """
-    version: str = "0.3"
+    version: str = "0.4"
 
     def __init__(
         self,
@@ -142,6 +142,6 @@ class AzStorageBlobReader(BaseReader):
             except ImportError:
                 SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
 
-            loader = SimpleDirectoryReader(temp_dir, file_extractor=self.file_extractor, num_files_limit=self.num_files_limit,)
+            loader = SimpleDirectoryReader(temp_dir, file_extractor=self.file_extractor, num_files_limit=self.num_files_limit, metadata=get_metadata)
 
             return loader.load_data()
